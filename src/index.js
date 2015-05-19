d2f9e70946cf2381b8835f41ce88ac18f476854d
Jason Kraus <zbyte64@gmail.com>
2015-05-19 16:09:07 -0700

    that was a bad way of specifying the url

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ export class Client {
   constructor(params = {}) {
     params.api_version = params.api_version || 1;
     this.params = params;
-    if(params.domain.match(/http/)) this.url_prefix = `${params.domain}/api/v${params.api_version}/`
-    else this.url_prefix = `http://${params.domain}.dev.montagehot.club/api/v${params.api_version}/`
+    if (this.params.url) {
+      this.url_prefix = this.params.url;
+    } else {
+      this.url_prefix = `http://${params.domain}.dev.montagehot.club/api/v${params.api_version}/`;
+    }
   }
   schemas() {
     return this.request(`schemas/`);
