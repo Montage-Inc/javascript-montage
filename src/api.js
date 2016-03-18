5374f0748b672a15c64e20ca04acb9fdabd71002
Nick Herrera <nherrera@editllc.com>
2016-03-18 03:03:27 -0700

    feat(RoleAPI): add `RoleAPI`

--- src/api.js
+++ src/api.js
@@ -115,3 +115,46 @@ export class UserAPI {
 		return this.client.request(`users/${user_id}/`, 'DELETE');
 	}
 }
+
+export class RoleAPI {
+	constructor(client) {
+		this.client = client;
+	}
+
+	create(name, add_users = []) {
+		const payload = { name, add_users };
+		return this.client.request(`roles/`, 'POST', payload);
+	}
+
+	list() {
+		return this.client.request(`roles/`);
+	}
+
+	get(role) {
+		return this.client.request(`roles/${role}/`);
+	}
+
+	update(role, name, add_users, remove_users) {
+		var payload = {};
+
+		if (name) {
+			payload.name = name;
+		}
+
+		if (add_users) {
+			payload.add_users = add_users;
+		}
+
+		if (remove_users) {
+			payload.remove_users = remove_users;
+		}
+
+		if (Object.keys(payload).length) {
+			return this.client.request(`roles/${role}/`, 'PATCH', payload);
+		}
+	}
+
+	remove(role) {
+		return this.client.request(`roles/${role}/`, 'DELETE');
+	}
+}
