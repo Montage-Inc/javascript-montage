ea045a724652372cdcdf74274d41713381c669e7
Nick Herrera <nherrera@editllc.com>
2016-03-18 02:36:08 -0700

    feat(SchemaAPI): add `SchemaAPI`

--- src/api.js
+++ src/api.js
@@ -33,3 +33,42 @@ export class DocumentsAPI {
 		return this.client.request(endpoint, 'DELETE');
 	}
 }
+
+export class SchemaAPI {
+	constructor(client) {
+		this.client = client;
+	}
+
+	create(name, fields = []) {
+		const payload = { name, fields };
+		return this.client.request('schemas/', 'POST', payload);
+	}
+
+	list() {
+		return this.client.request('schemas/');
+	}
+
+	get(schema) {
+		return this.client.request(`schemas/${schema}/`);
+	}
+
+	update(schema, name, fields) {
+		var payload = {};
+
+		if (name) {
+			payload.name = name;
+		}
+
+		if (fields) {
+			payload.fields = fields;
+		}
+
+		if (Object.keys(payload).length) {
+			return this.client.request(`schemas/${schema}/`, 'PATCH', payload);
+		}
+	}
+
+	remove(schema) {
+		return this.client.request(`schemas/${schema}/`, 'DELETE');
+	}
+}
