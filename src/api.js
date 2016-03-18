80029f979b60d1a89da010e33a5c5bda07bd0a63
Nick Herrera <nherrera@editllc.com>
2016-03-18 03:44:26 -0700

    feat(FileAPI): add `FileAPI`

--- src/api.js
+++ src/api.js
@@ -34,6 +34,29 @@ export class DocumentsAPI {
 	}
 }
 
+export class FileAPI {
+	constructor(client) {
+		this.client = client;
+	}
+
+	list() {
+		return this.client.request('files/');
+	}
+
+	get(file_id) {
+		return this.client.request(`files/${file_id}/`);
+	}
+
+	remove(file_id) {
+		const endpoint = `files/${file_id}/`;
+		return this.client.request(endpoint, 'DELETE');
+	}
+
+	save(formData) {
+		return this.client.request('files/', 'POST', formData, true);
+	}
+}
+
 export class SchemaAPI {
 	constructor(client) {
 		this.client = client;
