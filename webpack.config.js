37c3301f3503ddf0ab8fa9d01839c2361a7194c2
Nick Herrera <nherrera@editllc.com>
2016-03-18 01:35:39 -0700

    chore(webpack): use tabs instead of spaces for indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 var webpack = require("webpack");
 
 module.exports = {
-  context: __dirname,
-  entry: './src/index.js',
-  output: {
-    libraryTarget: "umd",
-    library: "Montage",
-  	filename: './dist/montage.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /(bower_components|node_modules)/,
-      query: {
-        plugins: ['transform-runtime'],
-        presets: ['es2015']
-      }
-    },
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ]
+	context: __dirname,
+	entry: './src/index.js',
+	output: {
+		libraryTarget: "umd",
+		library: "Montage",
+		filename: './dist/montage.js'
+	},
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			loader: 'babel-loader',
+			exclude: /(bower_components|node_modules)/,
+			query: {
+				plugins: ['transform-runtime'],
+				presets: ['es2015']
+			}
+		},
+		]
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.UglifyJsPlugin({minimize: true})
+	]
 }
