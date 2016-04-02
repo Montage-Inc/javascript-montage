19e977c6ad1da91bcefa558d290135d1328f7073
Nick Herrera <nherrera@editllc.com>
2016-04-02 15:28:28 -0700

    chore(build): use tabs instead of spaces

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,25 @@ var mocha = require('gulp-mocha');
 var webpack = require('gulp-webpack');
 
 
-gulp.task('build', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(to5({stage: 1, optional: 'runtime'}))
-    .pipe(gulp.dest('dist'));
+gulp.task('build', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(to5({stage: 1, optional: 'runtime'}))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', function() {
-  require("babel/register")({
-    stage: 1, optional: 'runtime'
-  });
+gulp.task('test', function () {
+	require("babel/register")({
+		stage: 1, optional: 'runtime'
+	});
 
-  return gulp.src('tests/**/*test.js')
-    .pipe(mocha({reporter: 'nyan'}));
+	return gulp.src('tests/**/*test.js')
+		.pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('webpack', function() {
-  var myConfig = Object.create(require('./webpack.config'));
+gulp.task('webpack', function () {
+	var myConfig = Object.create(require('./webpack.config'));
 
-  return gulp.src('src/')
-    .pipe(webpack(myConfig))
-    .pipe(gulp.dest('./'));
+	return gulp.src('src/')
+		.pipe(webpack(myConfig))
+		.pipe(gulp.dest('./'));
 });
