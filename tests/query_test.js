12ff8670791ae016dd099dcdee173fd6f4f77d58
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-01 21:59:10 -0700

    test(query): update tests to work with new object parameters in `query.js` - orderBy, between, getIntersecting, and getNearest still not passing

--- tests/query_test.js
+++ tests/query_test.js
@@ -17,6 +17,7 @@ describe('Query', () => {
 	describe('#toJS()', () => {
 		it('should return a query object', () => {
 			expect(query.toJS()).to.eql({
+				$type: 'query',
 				$schema: 'testSchema',
 				$query: []
 			});
@@ -26,28 +27,28 @@ describe('Query', () => {
 	describe('#get()', () => {
 		it('sets get', () => {
 			query.get('1234');
-			expect(query.terms).to.eql([['$get', '1234']]);
+			expect(query.terms).to.eql([['$get', {'id': '1234'}]]);
 		});
 	});
 
 	describe('#getAll()', () => {
 		it('sets getAll', () => {
 			query.getAll(['1234', 'abcd']);
-			expect(query.terms).to.eql([['$get_all', ['id', ['1234', 'abcd']]]]);
+			expect(query.terms).to.eql([['$get_all', [{'ids': ['1234', 'abcd'], 'index': 'id'}]]]);
 		});
 	});
 
 	describe('#hasFields()', () => {
 		it('sets hasFields', () => {
 			query.hasFields('title', 'rating');
-			expect(query.terms).to.eql([['$has_fields', ['title', 'rating']]]);
+			expect(query.terms).to.eql([['$has_fields', {fields: ['title', 'rating']}]]);
 		});
 	});
 
 	describe('#withFields()', () => {
 		it('sets withFields', () => {
 			query.withFields('title', 'rating');
-			expect(query.terms).to.eql([['$with_fields', ['title', 'rating']]]);
+			expect(query.terms).to.eql([['$with_fields', {fields: ['title', 'rating']}]]);
 		});
 	});
 
@@ -61,14 +62,14 @@ describe('Query', () => {
 
 		context('when direction is ascending', () => {
 			it('sets order_by and ascending direction', () => {
-				query.orderBy('rating', 'asc');
+				query.orderBy('rating', '$asc');
 				expect(query.terms).to.eql([['$order_by', 'rating', '$asc']]);
 			});
 		});
 
 		context('when direction is descending', () => {
 			it('sets order_by and descending direction', () => {
-				query.orderBy('rating', 'desc');
+				query.orderBy('rating', '$desc');
 				expect(query.terms).to.eql([['$order_by', 'rating', '$desc']]);
 			});
 		});
@@ -83,35 +84,35 @@ describe('Query', () => {
 	describe('#skip()', () => {
 		it('sets skip', () => {
 			query.skip(10);
-			expect(query.terms).to.eql([['$skip', 10]]);
+			expect(query.terms).to.eql([['$skip', {'n': 10}]]);
 		});
 	});
 
 	describe('#limit()', () => {
 		it('sets limit', () => {
 			query.limit(10);
-			expect(query.terms).to.eql([['$limit', 10]]);
+			expect(query.terms).to.eql([['$limit', {'n': 10}]]);
 		});
 	});
 
 	describe('#slice()', () => {
 		it('sets slice', () => {
 			query.slice(10, 20);
-			expect(query.terms).to.eql([['$slice', [10, 20]]]);
+			expect(query.terms).to.eql([['$slice', [{'startOffset': 10, 'endOffset': 20}]]]);
 		});
 	});
 
 	describe('#nth()', () => {
 		it('sets nth', () => {
 			query.nth(5);
-			expect(query.terms).to.eql([['$nth', 5]]);
+			expect(query.terms).to.eql([['$nth', {'n': 5}]]);
 		});
 	});
 
 	describe('#sample()', () => {
 		it('sets sample', () => {
 			query.sample(20);
-			expect(query.terms).to.eql([['$sample', 20]]);
+			expect(query.terms).to.eql([['$sample', {'n': 20}]]);
 		});
 	});
 
@@ -125,7 +126,7 @@ describe('Query', () => {
 	describe('#without()', () => {
 		it('sets without', () => {
 			query.without('votes', 'rank');
-			expect(query.terms).to.eql([['$without', ['votes', 'rank']]]);
+			expect(query.terms).to.eql([['$without', {'fields': ['votes', 'rank']}]]);
 		});
 	});
 
@@ -139,28 +140,28 @@ describe('Query', () => {
 	describe('#sum()', () => {
 		it('sets sum', () => {
 			query.sum('rank');
-			expect(query.terms).to.eql([['$sum', 'rank']]);
+			expect(query.terms).to.eql([['$sum', {'field': 'rank'}]]);
 		});
 	});
 
 	describe('#avg()', () => {
 		it('sets avg', () => {
 			query.avg('rank');
-			expect(query.terms).to.eql([['$avg', 'rank']]);
+			expect(query.terms).to.eql([['$avg', {'field': 'rank'}]]);
 		});
 	});
 
 	describe('#min()', () => {
 		it('sets min', () => {
 			query.min('rating');
-			expect(query.terms).to.eql([['$min', 'rating']]);
+			expect(query.terms).to.eql([['$min', {'field': 'rating'}]]);
 		});
 	});
 
 	describe('#max()', () => {
 		it('sets max', () => {
 			query.max('rating');
-			expect(query.terms).to.eql([['$max', 'rating']]);
+			expect(query.terms).to.eql([['$max', {'field': 'rating'}]]);
 		});
 	});
 
@@ -174,11 +175,13 @@ describe('Query', () => {
 	describe('#getIntersecting()', () => {
 		it('sets get_intersecting', () => {
 			var point = {
-	            'type': 'Point',
-	            'coordinates': [-120.34589052200315, 36.12704320788633]
-	        };
+				'geometry': {
+					'coordinates': [-120.34589052200315, 36.12704320788633],
+					'type': 'Point'
+				}
+			};
 			query.getIntersecting(point, 'location');
-			expect(query.terms).to.eql([['$get_intersecting', ['location', point]]]);
+			expect(query.terms).to.eql([['$get_intersecting', [{'index': 'location', point}]]]);
 		});
 	});
 
