101bc0dc8215997f807ada7de4b745f720ac8438
Elanna Grossman <elanna.grossman@gmail.com>
2016-08-03 10:47:11 -0700

    feat(tasks): add TaskAPI script to match the `python-montage` wrapper

---
+++ src/api/task.js
@@ -0,0 +1,25 @@
+export default class TaskAPI {
+	constructor(client) {
+		this.client = client;
+	}
+
+	run(command, image, token, timeLimit) {
+		const payload = { command };
+
+		if (image) {
+			payload.image = image;
+		}
+		if (token) {
+			payload.token = token;
+		}
+		if (timeLimit) {
+			payload.timeLimit = timeLimit;
+		}
+
+		return this.client.request(`tasks/`, 'POST', payload);
+	}
+
+	get(task_id) {
+		return this.client.request(`tasks/${task_id}/`);
+	}
+}
