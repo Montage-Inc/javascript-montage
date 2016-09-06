9745c438dd058a3b451e2dead4cfa18ef287106a
Elanna Grossman <elanna.grossman@gmail.com>
2016-09-06 11:30:29 -0700

    feat(project): add `ProjectAPI` script

---
+++ src/api/project.js
@@ -0,0 +1,25 @@
+export default class ProjectAPI {
+	constructor(client) {
+		this.client = client;
+	}
+
+	get() {
+		return this.client.request(`project`);
+	}
+
+	update(name, subdomain) {
+		var payload = {};
+
+		if(name) {
+			payload.name = name;
+		}
+
+		if(subdomain) {
+			payload.subdomain = subdomain;
+		}
+
+		if(payload) {
+			return this.client.request(`project`, 'PATCH', payload);
+		}
+	}
+}
